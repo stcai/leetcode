589929854c819d38c636a2fe04c2689648dc74a8
Felix Cai <stcai80@gmail.com>
2021-12-27 01:34:30 +0800

    77.combinations

---
+++ algorithms/00077.combinations/cpp/solution.hpp
@@ -0,0 +1,30 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+  vector<vector<int>> combine(int n, int k) {
+    vector<vector<int>> result;
+    vector<int> combination(k);
+    int count = 0;
+    backtrack(n, k, 1, combination, count, result);
+    return result;
+  }
+
+ private:
+  void backtrack(int n, int k, int level, vector<int>& combination, int& count,
+                 vector<vector<int>>& result) {
+    if (count == k) {
+      result.push_back(combination);
+      return;
+    }
+
+    for (int i = level; i <= n; ++i) {
+      combination[count] = i;
+      ++count;
+      backtrack(n, k, i + 1, combination, count, result);
+      --count;
+    }
+  }
+};
