19f1e6cad3bcf870976c7f75a891f3bf32c987ca
Felix Cai <stcai80@gmail.com>
2021-12-30 00:59:10 +0800

    tweak 79.word-search

--- algorithms/00079.word-search/cpp/solution.hpp
+++ algorithms/00079.word-search/cpp/solution.hpp
@@ -12,8 +12,7 @@ class Solution {
     const int n = board[0].size();
     for (int i = 0; i < m; ++i) {
       for (int j = 0; j < n; ++j) {
-        bool exist = false;
-        search(board, i, j, word, 0, visited, exist);
+        bool exist = search(board, i, j, word, 0, visited);
         if (exist) {
           return true;
         }
@@ -23,24 +22,24 @@ class Solution {
   }
 
  private:
-  void search(const vector<vector<char>>& board, int row, int column,
-              const string& word, int word_index, vector<vector<bool>>& visited,
-              bool& exist) {
+  bool search(const vector<vector<char>>& board, int row, int column,
+              const string& word, int word_index,
+              vector<vector<bool>>& visited) {
     if ((row >= board.size() || column >= board[0].size()) ||
-        (board[row][column] != word[word_index]) || visited[row][column] ||
-        exist) {
-      return;
+        (board[row][column] != word[word_index]) || visited[row][column]) {
+      return false;
     }
     if (word_index == word.size() - 1) {
-      exist = true;
-      return;
+      return true;
     }
 
     visited[row][column] = true;
-    search(board, row - 1, column, word, word_index + 1, visited, exist);
-    search(board, row + 1, column, word, word_index + 1, visited, exist);
-    search(board, row, column - 1, word, word_index + 1, visited, exist);
-    search(board, row, column + 1, word, word_index + 1, visited, exist);
+    bool is_exist =
+        search(board, row - 1, column, word, word_index + 1, visited) ||
+        search(board, row + 1, column, word, word_index + 1, visited) ||
+        search(board, row, column - 1, word, word_index + 1, visited) ||
+        search(board, row, column + 1, word, word_index + 1, visited);
     visited[row][column] = false;
+    return is_exist;
   }
 };
